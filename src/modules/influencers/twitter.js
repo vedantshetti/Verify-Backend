9fe4ed4cb0070e3b7d4ad6e3cbd351cac2723593
vedantshetti <vedantshetti123456@gmail.com>
2025-07-07 13:14:34 +0530

    updated the model and the twitter for the purpose of handling twitter handle

--- src/modules/influencers/twitter.js
+++ src/modules/influencers/twitter.js
@@ -2,20 +2,31 @@ const axios = require("axios");
 
 const BEARER_TOKEN = process.env.TWITTER_BEARER_TOKEN; // Set in .env
 
+// Fetch latest Twitter profile info (bio, image, etc.)
+async function fetchTwitterProfile(username) {
+  const url = `https://api.twitter.com/2/users/by/username/${username}?user.fields=profile_image_url,description,public_metrics`;
+  const res = await axios.get(url, {
+    headers: { Authorization: `Bearer ${BEARER_TOKEN}` },
+  });
+  const user = res.data.data;
+  return {
+    image: user.profile_image_url,
+    bio: user.description,
+    followers: user.public_metrics.followers_count,
+  };
+}
+
+// Fetch recent tweets (if needed)
 async function fetchRecentTweets(username, maxResults = 100) {
-  // 1. Get user ID from username
   const userRes = await axios.get(
     `https://api.twitter.com/2/users/by/username/${username}`,
     { headers: { Authorization: `Bearer ${BEARER_TOKEN}` } }
   );
   const userId = userRes.data.data.id;
-
-  // 2. Get up to 100 recent tweets
   const tweetsRes = await axios.get(
     `https://api.twitter.com/2/users/${userId}/tweets?max_results=${maxResults}&tweet.fields=created_at,public_metrics`,
     { headers: { Authorization: `Bearer ${BEARER_TOKEN}` } }
   );
-
   return tweetsRes.data.data.map((tweet) => ({
     id: tweet.id,
     text: tweet.text,
@@ -28,4 +39,4 @@ async function fetchRecentTweets(username, maxResults = 100) {
   }));
 }
 
-module.exports = { fetchRecentTweets };
+module.exports = { fetchTwitterProfile, fetchRecentTweets };
